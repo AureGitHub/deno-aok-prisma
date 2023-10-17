3680cda6f170c7b398c619924dddadba3434948b
AureGitHub <auredeveloper@gmail.com>
2023-10-17 16:41:30 +0200

    Update server.ts  res.header("Access-Control-Allow-Origin", "*");

--- server.ts
+++ server.ts
@@ -17,7 +17,13 @@ const app = new Application();
 const router = new Router();
 
 
-app.use(oakCors({ origin: "*" }));
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
+//app.use(oakCors({ origin: "*" }));
 
 
 // Middleware Logger
