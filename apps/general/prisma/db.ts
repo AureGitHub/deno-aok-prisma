ddbbfe0e8bc1e5540c9f19bc9efb4eae72eb21e1
AureGitHub <auredeveloper@gmail.com>
2024-01-04 10:14:49 +0100

    ..

--- apps/general/prisma/db.ts
+++ apps/general/prisma/db.ts
@@ -2,12 +2,14 @@ import { PrismaClient } from "../generated/client/deno/edge.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
 
 
-const { DATABASE_URL_GENERAL } = Deno.env.toObject();
+// const { DATABASE_URL_GENERAL } = Deno.env.toObject();
+const { DATABASE_PRISMA_TTEC_EUROMILLONES } = Deno.env.toObject();
+
 
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: "DATABASE_URL_GENERAL"
+      url: DATABASE_PRISMA_TTEC_EUROMILLONES
     },
   },
 });
