54fcac88915b0c8008d88298a1beb017fd1bec31
AureGitHub <auredeveloper@gmail.com>
2024-02-28 10:52:04 +0100

    correccion apuesta y ionic con administracion

--- apps/ttec-euromillones/entity/Administracion/controller.ts
+++ apps/ttec-euromillones/entity/Administracion/controller.ts
@@ -15,18 +15,50 @@ const get= async (ctx: any) => {
 
 
   const entityUser = new aureDB(client, entities, 'User');
+  const entityApuesta = new aureDB(client, entities, 'Apuesta');
+
+
   const users = await entityUser.findMany({where: {estadoid: TC_UserEstado.activo }});
 
   const totalUsers = users.length;
   const lstAllEmails = users.map((a)=> a.email).join(',');
 
-  const lstAllEmailsInRed = users.filter(a=> a.saldo < 1).map((a)=> a.email).join(',');
+  let lstUserInConflictos = users.filter(a=> a.saldo <= 2);
+
+  lstUserInConflictos = lstUserInConflictos.sort((a,b)=> a.saldo - b.saldo);
+
+  const sqlSelectApuesta = ` select  fecha, ganado,apostado  from "Apuesta" a 
+  where estadoid = 3
+  order by id desc
+  limit 1 `;
+
+
+  let fechaUltimaApuesta = '';
+  let ganadoUltimaApuesta = '';
+  let apostadoUltimaApuesta = '';
+  
+
+  const result=await client.queryObject(sqlSelectApuesta);
+
+  if(result.rowCount == 1){
+    fechaUltimaApuesta = new Date(result.rows[0].fecha).toLocaleDateString();
+    ganadoUltimaApuesta = result.rows[0].ganado;
+    apostadoUltimaApuesta = result.rows[0].apostado;
+    if(apostadoUltimaApuesta.indexOf('.')>0){
+      apostadoUltimaApuesta=apostadoUltimaApuesta.split('.')[0];
+    }
+
+  }
+
+
+  
+
 
    
   ctx.response.status = 201;
    ctx.response.body = {
      status: StatusCodes.OK,
-     data: { data:{ totalUsers, lstAllEmails, lstAllEmailsInRed}
+     data: { data:{ totalUsers, lstAllEmails,  fechaUltimaApuesta, ganadoUltimaApuesta, apostadoUltimaApuesta, lstUserInConflictos}
         },
    };
 
